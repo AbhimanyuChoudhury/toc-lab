256dd6e7f49a55fa735c2475e70c865a6d578a6a
koushik1 <koushik1@users.noreply.github.com>
2017-03-23 01:56:43 +0530

    Update ques3.c

--- ques3.c
+++ ques3.c
@@ -1,100 +1,93 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-#define NO_ITEMS 7
+#define MAX 100
 
-int prices[] = {10,25,5,7,9,13,20};
-int currency[] = {1,5,10};
+typedef struct stack
+{
+	char data[MAX];
+	int top;
+}stack;
 
+stack s;
 
-void startState();
-void incompleteState(int itemChoice,int paid);
-void invalidState(int itemChoice, int paid);
-void finishingState();
-void finishedState();
+void push(char data){
+	s.data[++s.top] = data;
+}
 
-
-void main(){
-
-printf("Items and their prices\n");
-int i;
-for(i=0;i<NO_ITEMS;i++)
-	printf("Item %d\tPrice %d\n",i,prices[i]);
-
-printf("Acceptable currency is 1,5,10\n");
-
-startState();
+char pop(){
+	return s.data[s.top--];
 }
 
 
-void startState(){
+int precedence(char c){
+	if(c == '(')
+		return 0;
+	if(c == '+' )
+		return 1;
+	if(c == '.' )
+		return 2;
+	else
+		return -1;
+}
 
-printf("Choose Item: ");
-int itemChoice;
-scanf("%d",&itemChoice);
-printf("Price to be paid: %d\n",prices[itemChoice]);
+int typeof1(char c){
 
-int paid=0;
-incompleteState(itemChoice,paid);
+	if(c == '(')
+		return 1;
+	if(c == ')')
+		return 2;
+	if(c == '+' || c == '.')
+		return 3;
+	if (c == ' ')
+		return 4;
+	else
+		return 5;
 }
 
 
+int main() {
 
-void incompleteState(int itemChoice, int paid){
+char array[20];	
+char p;
+char *c;
 
-//int paid=0;
+s.top=-1;
 
-while(paid<prices[itemChoice]){
-	printf("Enter Coin:");
-	int coin;
-	scanf("%d",&coin);
+printf("Enter the infix expression");
+scanf("%s",array);
+c = array;
 
-	if(coin==1){
-		paid = paid+1;
-	}
-	else if(coin==5){
-		paid = paid+5;
-	}
-	else if(coin==10){
-		paid= paid+10;
-	}
+while(*c != '\0'){
 
-	else{
-		invalidState(itemChoice,paid);
-		break;
-	}
+	int x = typeof1(*c);
+	
+	if(x==5)
+		printf("%c",*c );
 
-	if(prices[itemChoice]-paid>0)
-		printf("Paid = %d,  Amount remaining = %d\n",paid,prices[itemChoice]-paid );
+	else if(x == 1)
+		push(*c);
+	
+	else if(x == 2){
+		while(( p = pop())!='(')
+       	 	printf("%c",p);
+    }
+ 
+    else {
 
+     	while(precedence(s.data[s.top]) >= precedence(*c) )
+      		printf("%c",pop());
+      	push(*c);
+    }
+
+	c++;
 }
 
-if(prices[itemChoice]-paid<=0){
-	if(prices[itemChoice]-paid<0)
-		printf("Your Change is: %d\n", paid-prices[itemChoice]);
-	finishingState();
-}
+while(s.top != -1){
+        printf("%c",pop());
+    }
+ 
 
-}
+printf("\n");
+return 0;
 
-void invalidState(int itemChoice, int paid){
-	printf("Please Enter Valid Coin\n");
-	incompleteState(itemChoice,paid);
-}
-
-
-
-void finishingState(){
-	printf("Thank You For Buying Our Item\n");
-	printf("Do You Want Another Item- Y(Yes) or N(No):");
-	char choice;
-	scanf(" %c",&choice);
-	if(choice=='Y')
-		startState();
-	if(choice=='N')
-		finishedState();
-}
-
-void finishedState(){
-	printf("Thank You! Have a nice day\n");
 }
